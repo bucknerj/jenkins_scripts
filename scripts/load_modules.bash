9ce78edcef13159cb24794bf21a7546ab03120b2
Josh Buckner <bucknerj@umich.edu>
2016-11-29 14:47:56 -0500

    no modules on trs-80 yet

--- scripts/load_modules.bash
+++ scripts/load_modules.bash
@@ -1,14 +1,14 @@
-export MODULESHOME=/usr/local/Modules/3.2.10
-source $MODULESHOME/init/sh
+# export MODULESHOME=/usr/local/Modules/3.2.10
+# source $MODULESHOME/init/sh
 
-module load OpenMM/6.3.0
-module load FFTW/3.3.5
+# module load OpenMM/6.3.0
+# module load FFTW/3.3.5
 
-if [[ "$1" == "em64t" ]]; then
-  module load Intel/2015
-  module load openmpi/2.0-intel-15
-else
-  module load openmpi/2.0-gcc-system
-fi
+# if [[ "$1" == "em64t" ]]; then
+#   module load Intel/2015
+#   module load openmpi/2.0-intel-15
+# else
+#   module load openmpi/2.0-gcc-system
+# fi
 
 # now relying on system to provide gfortran, fftw, and openmpi-gcc
