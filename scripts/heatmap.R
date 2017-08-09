94b8438e7fe0ed3dff14fe6f783244f90f549c56
Josh Buckner <bucknerj@umich.edu>
2017-08-09 14:47:34 -0400

    heatmap R script improvements

--- scripts/heatmap.R
+++ scripts/heatmap.R
@@ -19,31 +19,19 @@ library(knitr, warn.conflicts = FALSE, quietly = TRUE)
 
 tdiff <- tbl_df(fread(args[1]))
 colnames(tdiff) <- c('test', 'date.a', 'date.b', 'edit.dist')
-# tdiff$test <- as.factor(tdiff$test)
-# tdiff$dates <- as.factor(tdiff$dates)
-tdiff <- tdiff[order(tdiff$test, tdiff$date.a)]
+setorder(tdiff, test, date.a)
 
-# kable(head(tdiffs))
+gg <- ggplot(tdiff, aes(x=date.a, y=test, fill=edit.dist)) +
+    geom_tile(color="white", size=0.1) +
+    scale_fill_viridis(name="edit dist", label=comma) +
+    coord_equal() +
+    labs(x=NULL, y=NULL, title="testcase changes") +
+    theme_tufte() +
+    theme(plot.title=element_text(hjust=0)) +
+    theme(axis.ticks=element_blank()) +
+    theme(axis.text=element_text(size=7)) +
+    theme(axis.text.x=element_text(angle=45, hjust=0.8)) +
+    theme(legend.title=element_text(size=8)) +
+    theme(legend.text=element_text(size=6))
 
-png(args[2]) #,    # create PNG for the heat map
-
-#    width = 10*300,        # 10 x 300 pixels
-#    height = 10*300,
-#    res = 300,            # 300 pixels per inch
-#    pointsize = 8)        # smaller font size
-
-gg <- ggplot(tdiff, aes(x=date.a, y=test, fill=edit.dist))
-gg <- gg + geom_tile(color="white", size=0.1)
-gg <- gg + scale_fill_viridis(name="edit dist", label=comma)
-gg <- gg + coord_equal()
-gg <- gg + labs(x=NULL, y=NULL, title="testcase changes")
-gg <- gg + theme_tufte()
-gg <- gg + theme(plot.title=element_text(hjust=0))
-gg <- gg + theme(axis.ticks=element_blank())
-gg <- gg + theme(axis.text=element_text(size=7))
-gg <- gg + theme(axis.text.x=element_text(angle=45, hjust=0.8))
-gg <- gg + theme(legend.title=element_text(size=8))
-gg <- gg + theme(legend.text=element_text(size=6))
-gg
-
-dev.off()               # close the PNG device
+ggsave(file=args[2], plot=gg)
