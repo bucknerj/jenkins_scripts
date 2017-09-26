240d6ca51c5d05804b847780776d0a0efc4ddea6
Josh Buckner <bucknerj@umich.edu>
2017-09-26 12:14:43 -0400

    grader: process all tests in parallel

--- scripts/grader.py
+++ scripts/grader.py
@@ -1,98 +1,19 @@
 from sys import argv, exit
 from itertools import dropwhile, filterfalse
 from difflib import IS_LINE_JUNK, SequenceMatcher, HtmlDiff
-from os.path import basename
+from os.path import basename, isfile, join
+from functools import partial
 import tempfile
 import subprocess
 import os
+import concurrent.futures
+
 
 def any_in(substrs, line):
     return any(True for _ in
                dropwhile(lambda l: not l in line, substrs))
 
-def is_junk_line(line):
-    to_remove = [
-        " Attempting to open",
-        " EEMA",
-        " Eigenvalue=",
-        " Improved step=",
-        " OPNLGU> ",
-        " Other: ",
-        " TIME= ",
-        " energ=",
-        " rnew=",
-        "! input data directory",
-        "! scratch directory",
-        "----------",
-        "ALLHP: ",
-        "ALLHP> ",
-        "CGONNB =",
-        "CGOFNB =",
-        " Version ",
-        "CHARMM>",
-        "CLCG Random",
-        "CMA: no RDMA devices found",
-        "COLLCT_FSTSHK",
-        "CPU TIME:",
-        "CPU TIME=",
-        "CREATED BY USER",
-        "Created on",
-        "Current HEAP",
-        "Current operating system",
-        "EANGLC: QANGTYPE =",
-        "EANGLE> FORCE: ANGLE NOT FLAT",
-        "EANGLE> Using CHARMM angle function",
-        "ELAPSED TIME:",
-        "Using routine",
-        "FCTINI> Surface tension coeff",
-        "FREHP: ",
-        "FREHP> ",
-        "FSSHKINI",
-        "Git commit ID",
-        "In OpenMM plugin directory",
-        "RESIZING",
-        "Maximum number of ATOMS:",
-        "NBLIST_BUILDER Allocating grid",
-        "NUMBER OF ENERGY EVALUATIONS",
-        "New timer profile",
-        "Number of lone-pairs",
-        "OpenMM initiated with",
-        "PARRDR> ALL ANGLES HAVE POSITIVE MINIMA",
-        "PRINHP> ",
-        "Parallel load balance",
-        "Parameter #",
-        "Parameter: ",
-        "Processing passed argument ",
-        "QM groups found:",
-        "QCHEM: Classical atoms excluded from the QM calculation",
-        "RANDOM NUM",
-        "RDCMND substituted parameter",
-        "Reaction field dielectric",
-        "SA modulation: ",
-        "SEEDS>",
-        "SVN revision",
-        "Splitting recip cores into (y by z):",
-        "TITLE>",
-        "TORQ> No external forces defined",
-        "TOTAL NUMBER OF CYCLES",
-        "There are no",
-        "Total heap storage needed ",
-        "Using Column FFT",
-        "Using FFTW",
-        "Using MKL",
-        "Using Pub FFT",
-        "VALB CPUC",
-        "VCLOSE:",
-        "coor. length ",
-        "allocated space for coor",
-        "emapwrite> ",
-        "libOmmXml",
-        "libOpenMM",
-        "operation not performed",
-        "random clcg",
-        "reference distance",
-        "ISEED ="
-    ]
+def is_junk_line(to_remove, line):
     return any_in(to_remove, line)
 
 def is_skip_line(l): 
@@ -105,8 +26,8 @@ def is_skip_line(l):
 def is_test_skipped(test_lines):
     return any(map(is_skip_line, test_lines))
 
-def create_filtered_file(lines):
-    f_lines = [l for l in lines if not is_junk_line(l)]
+def create_filtered_file(to_remove, lines):
+    f_lines = [l for l in lines if not is_junk_line(to_remove, l)]
     f = tempfile.NamedTemporaryFile()
     for l in f_lines:
         f.write(l.encode())
@@ -115,65 +36,132 @@ def create_filtered_file(lines):
     os.fsync(f)
     return f
 
-current_test_file = open(argv[1], errors = 'replace')
-current_test_lines = current_test_file.readlines()
-current_test_file.close()
+def grade_test(test_lines):
+    report_lines = []
+    if is_test_skipped(test_lines):
+        report_lines.append("  <skipped />")
+    elif any("NO TERMINATION" in l for l in test_lines):
+        report_lines.append("  <failure>")
+        report_lines.append("    NO TERMINATION")
+        report_lines.append("  </failure>")
+    elif any("ABNORMAL TERMINATION" in l for l in test_lines):
+        report_lines.append("  <failure>")
+        report_lines.append("    ABNORMAL TERMINATION")
+        report_lines.append("  </failure>")
 
-test_name = basename(argv[1]).split('.')[0]
-print("<testcase name=", test_name, '>', sep = '')
+    return report_lines
 
-if is_test_skipped(current_test_lines):
-    print("  <skipped />")
-    print("</testcase>")
-    exit(0)
-elif any("NO TERMINATION" in l for l in current_test_lines):
-    print("  <failure>")
-    print("    NO TERMINATION")
-    print("  </failure>")
-    print("</testcase>")
-    exit(0)
-elif any("ABNORMAL TERMINATION" in l for l in current_test_lines):
-    print("  <failure>")
-    print("    ABNORMAL TERMINATION")
-    print("  </failure>")
-    print("</testcase>")
-    exit(0)
+def compare_files(to_remove, old_test_lines, new_test_lines):
+    old_file = create_filtered_file(to_remove, old_test_lines)
+    new_file = create_filtered_file(to_remove, new_test_lines)
 
-curr_file = create_filtered_file(current_test_lines)
-
-previous_test_file = open(argv[2], errors = 'replace')
-previous_test_lines = previous_test_file.readlines()
-previous_test_file.close()
-
-prev_file = create_filtered_file(previous_test_lines)
-
-diff_result = subprocess.run(['diff', '-qiEbwB', prev_file.name, curr_file.name],
-                             stdout = subprocess.PIPE)
-
-if diff_result.returncode != 0:
-    diff_proc = subprocess.Popen(['diff',
-                                  '-yiEbwB',
-                                  '--suppress-common-lines',
-                                  prev_file.name,
-                                  curr_file.name],
+    diff_result = subprocess.run(['diff', '-qiEbwB',
+                                  old_file.name,
+                                  new_file.name],
                                  stdout = subprocess.PIPE)
-    head_proc = subprocess.Popen(['head'],
-                                 stdin = diff_proc.stdout,
-                                 stdout = subprocess.PIPE)
-    diff_proc.stdout.close()
-    diff_lines = head_proc.communicate()[0]
+    report_lines = []
+    if diff_result.returncode != 0:
+        diff_proc = subprocess.Popen(['diff',
+                                      '-yiEbwB',
+                                      '--suppress-common-lines',
+                                      old_file.name,
+                                      new_file.name],
+                                     stdout = subprocess.PIPE)
+        head_proc = subprocess.Popen(['head'],
+                                     stdin = diff_proc.stdout,
+                                     stdout = subprocess.PIPE)
+        diff_proc.stdout.close()
+        diff_lines = head_proc.communicate()[0]
+        
+        report_lines.append("  <failure>")
+        report_lines.extend(diff_lines.decode(errors = 'replace').split(os.linesep))
+        report_lines.append("  </failure>")
 
-    print("  <failure>")
-    print(diff_lines.decode(errors = 'replace'))
-    print("  </failure>")
+    old_file.close()
+    new_file.close()
+    return report_lines
 
-curr_file.close()
-prev_file.close()
-print("</testcase>")
+def process_test(to_remove, old_dir, old_fns, new_dir, new_fn):
+    new_test_file = open(join(new_dir, new_fn), errors = 'replace')
+    new_test_lines = new_test_file.readlines()
+    new_test_file.close()
+    
+    self_report = grade_test(new_test_lines)
+    
+    test_name = basename(new_fn).split('.')[0]
 
-# p1 = Popen(["dmesg"], stdout=PIPE)
-# p2 = Popen(["grep", "hda"], stdin=p1.stdout, stdout=PIPE)
-# p1.stdout.close()  # Allow p1 to receive a SIGPIPE if p2 exits.
-# output = p2.communicate()[0]
+    new_results = list()
+    new_results.append('<testcase name=' + test_name + '>')
+
+    if self_report:
+        new_results.extend(self_report)
+        new_results.append('</testcase>')
+        return new_results
+
+    if not (new_fn in old_fns):
+        new_results.extend([
+            '  <failure>',
+            '    NEW TEST',
+            '  </failure>',
+            '</testcase>'])
+        return new_results
+        
+    old_test_file = open(join(old_dir, new_fn), errors = 'replace')
+    old_test_lines = old_test_file.readlines()
+    old_test_file.close()
+
+    report_lines = compare_files(to_remove, old_test_lines, new_test_lines)
+    
+    if report_lines:
+        new_results.extend(report_lines)
+            
+    new_results.append('</testcase>')
+    return new_results
+
+def print_results(results):
+    test_names = sorted(results.keys())
+    for t in test_names:
+        for l in results[t]:
+            print(l)
+
+def main():
+    if len(argv) < 4:
+        print('Usage:', argv[0],
+              '<bad patterns file>', '<old test dir>', '<new test dir>')
+        exit(1)
+        
+    to_remove_fn = argv[1]
+    old_dir = argv[2]
+    new_dir = argv[3]
+
+    new_fns = [fn for fn in os.listdir(new_dir)
+                    if isfile(join(new_dir, fn))
+                    and fn.endswith(".out")]
+    old_fns = [fn for fn in os.listdir(old_dir)
+                    if isfile(join(old_dir, fn))
+                    and fn.endswith(".out")]
+
+    to_remove = []
+    with open(to_remove_fn) as to_remove_f:
+        to_remove.extend([l.strip() for l in to_remove_f 
+                          if l.strip() and not l.startswith('#')])
+
+    test_to_file = dict()
+    for new_fn in new_fns:
+        test_name = basename(new_fn).split('.')[0]
+        test_to_file[test_name] = new_fn
+
+    results = []
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        results = zip(test_to_file.keys(),
+                      executor.map(partial(process_test,
+                                           to_remove,
+                                           old_dir, old_fns,
+                                           new_dir),
+                                   test_to_file.values()))
                                
-# output=check_output("dmesg | grep hda", shell=True)
+    print_results(dict(results))
+    exit(0)
+
+if __name__ == '__main__':
+    main()
