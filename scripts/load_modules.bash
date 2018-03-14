7a4fcaee0be7be51def8f6ada91413897e251d29
Josh Buckner <bucknerj@umich.edu>
2018-03-14 16:30:16 -0400

    clean up modules in use

--- scripts/load_modules.bash
+++ scripts/load_modules.bash
@@ -1,12 +1,9 @@
-# export MODULESHOME=/usr/local/Modules/3.2.10
-# source $MODULESHOME/init/sh
-
-# module load OpenMM/6.3.0
-# module load FFTW/3.3.5
-
-# now relying on system to provide gfortran, fftw, and openmpi-gcc
 . /opt/rh/rh-python35/enable
 . /etc/profile.d/modules.sh
+
+module load cuda/9.1
+module load openmm/7.2
+
 if [[ "$1" == "em64t" ]]; then
     module load intel/16.x
     module load mpi/2.0-intel-16
@@ -17,5 +14,4 @@ else
     module load mpi/1.10.5
 fi
 
-export PATH=/usr/local/cuda/bin:$PATH
 export FFTW_HOME=/usr
