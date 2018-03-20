bd97bb25a06ef962a9f20c404cdead8b352db5c8
Josh Buckner <bucknerj@umich.edu>
2018-03-20 10:46:03 -0400

    cuda 9 only supports intel 17 for host compiler

--- scripts/load_modules.bash
+++ scripts/load_modules.bash
@@ -7,7 +7,7 @@ module load openmm/7.2
 if [[ "$1" == "em64t" ]]; then
     module load intel/18.x
     module load mpi/3.0-intel-18
-    export CUDA_HOST_COMPILER=/opt/intel/bin/icpc
+    export CUDA_HOST_COMPILER=/usr/bin/g++
 elif [[ "$1" == "cmake" ]]; then
     . /opt/rh/devtoolset-7/enable
     module load mpi/3.0-gcc-7.2
