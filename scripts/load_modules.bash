453eebab1f31813d778ce6c633a72dabcf97fbe0
Josh Buckner <bucknerj@umich.edu>
2018-03-20 10:47:13 -0400

    simplify script

--- scripts/load_modules.bash
+++ scripts/load_modules.bash
@@ -7,13 +7,12 @@ module load openmm/7.2
 if [[ "$1" == "em64t" ]]; then
     module load intel/18.x
     module load mpi/3.0-intel-18
-    export CUDA_HOST_COMPILER=/usr/bin/g++
 elif [[ "$1" == "cmake" ]]; then
     . /opt/rh/devtoolset-7/enable
     module load mpi/3.0-gcc-7.2
-    export CUDA_HOST_COMPILER=/usr/bin/g++
 else
     module load mpi/1.10.5
 fi
 
+export CUDA_HOST_COMPILER=/usr/bin/g++
 export FFTW_HOME=/usr
