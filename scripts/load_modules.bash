db00ec77998006be1d612d250f1c7f5047d8be2b
Josh Buckner <bucknerj@umich.edu>
2018-03-20 09:48:37 -0400

    update intel modules

--- scripts/load_modules.bash
+++ scripts/load_modules.bash
@@ -5,14 +5,15 @@ module load cuda/9.1
 module load openmm/7.2
 
 if [[ "$1" == "em64t" ]]; then
-    module load intel/16.x
-    module load mpi/2.0-intel-16
-elif [[ "$1" == "cmake" ]]; then 
-    module load mpi/3.0-gcc-7.2
+    module load intel/18.x
+    module load mpi/3.0-intel-18
+    export CUDA_HOST_COMPILER=/opt/intel/bin/icpc
+elif [[ "$1" == "cmake" ]]; then
     . /opt/rh/devtoolset-7/enable
+    module load mpi/3.0-gcc-7.2
+    export CUDA_HOST_COMPILER=/usr/bin/g++
 else
     module load mpi/1.10.5
 fi
 
 export FFTW_HOME=/usr
-export CUDA_HOST_COMPILER=/usr/bin/g++
