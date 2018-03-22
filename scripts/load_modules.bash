81c15c05ad8341444720d1ac84a9cab95cd9c1e5
Josh Buckner <bucknerj@umich.edu>
2018-03-22 14:15:19 -0400

    install.com cannot compile plugins w omm 7.2

--- scripts/load_modules.bash
+++ scripts/load_modules.bash
@@ -2,15 +2,17 @@
 . /etc/profile.d/modules.sh
 
 module load cuda/9.1
-module load openmm/7.2
 
 if [[ "$1" == "em64t" ]]; then
+    module load openmm/7.1
     module load intel/16.x
     module load mpi/2.0-intel-16
 elif [[ "$1" == "cmake" ]]; then 
+    module load openmm/7.2
     module load mpi/3.0-gcc-7.2
     . /opt/rh/devtoolset-7/enable
 else
+    module load openmm/7.1
     module load mpi/1.10.5
 fi
 
