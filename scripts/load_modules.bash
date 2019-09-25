35b430add3d2ec003f4a770e12cf83ca5e2f55a2
Josh Buckner <bucknerj@umich.edu>
2019-09-25 14:15:00 -0400

    update env modules used for testing

--- scripts/load_modules.bash
+++ scripts/load_modules.bash
@@ -6,18 +6,18 @@ module load cuda/system
 export FFTW_HOME=/usr
 
 if [[ "$1" == "em64t" ]]; then
-    module load openmm/7.1
+    module load openmm/7.4
     module load intel/18.x
     module load mpi/3.0-intel-18
 elif [[ "$1" == "cmake" ]]; then 
-    module load openmm/7.2
-    . /opt/rh/devtoolset-7/enable
-    module load mpi/3.1-gcc-7.3
+    module load openmm/7.4
+    . /opt/rh/devtoolset-8/enable
+    module load mpi/4.0-scl-8.3
 elif [[ "$1" == "pgi" ]]; then 
     . /opt/rh/devtoolset-7/enable
     module load openmm/7.2
     module load pgi/18.4
 else
-    module load openmm/7.1
+    module load openmm/7.4
     module load mpi/1.10.5
 fi
