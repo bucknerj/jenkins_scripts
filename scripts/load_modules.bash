c1ffc4080412875bb1b8ab0a52d77e806f2626dc
Josh Buckner <bucknerj@umich.edu>
2020-01-30 14:18:31 -0500

    added some debugging code

--- scripts/load_modules.bash
+++ scripts/load_modules.bash
@@ -5,19 +5,25 @@ module load cuda/system
 
 export FFTW_HOME=/usr
 
+echo "build type |$1|"
+
 if [[ "$1" == "em64t" ]]; then
+    echo "loading modules for intel build"
     module load openmm/7.4
     module load intel/18.x
     module load mpi/3.0-intel-18
-elif [[ "$1" == "cmake" ]]; then 
+elif [[ "$1" == "cmake" ]]; then
+    echo "loading modules for gnu build"
     module load openmm/7.4
     . /opt/rh/devtoolset-8/enable
     module load mpi/4.0-scl-8.3
-elif [[ "$1" == "pgi" ]]; then 
+elif [[ "$1" == "pgi" ]]; then
+    echo "loading modules for pgi build"
     . /opt/rh/devtoolset-7/enable
     module load openmm/7.2
     module load pgi/18.4
 else
+    echo "loading modules for unknown build"
     module load openmm/7.4
     module load mpi/1.10.5
 fi
