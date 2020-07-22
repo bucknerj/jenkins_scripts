b72e3fe1b4d777e0990354cf0a9488adbfd76a70
Josh Buckner <bucknerj@umich.edu>
2020-07-22 14:29:40 -0400

    blade test cases want only 1 node

--- scripts/dev.groovy
+++ scripts/dev.groovy
@@ -23,7 +23,7 @@ def cmakeBuilds =
   [ [name: 'lite', build: '--lite -g', test: 'cmake']
   , [name: 'openmm', build: '', test: 'cmake']
   , [name: 'domdec_gpu', build: '-u --with-gcc', test: 'M 2 X 2 cmake']
-  , [name: 'blade', build: '-u --with-blade --with-gcc', test: 'M 2 X 2 cmake']
+  , [name: 'blade', build: '-u --with-blade --with-gcc', test: 'cmake']
   , [name:'intel', build:'--with-intel', test:'M 2 X 2 cmake']
   , [name:'sccdftb' , build:'--with-sccdftb' , test:'cmake']
   , [name:'repdstr' , build:'--with-repdstr' , test:'M 2 X 2 cmake']
