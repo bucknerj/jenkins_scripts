9cceedd95d0d83d68ab9d952f98d4f068ebfb6a8
Josh Buckner <bucknerj@umich.edu>
2022-01-22 11:20:15 -0500

    initial commit of job descriptions for Bernies branch

---
+++ scripts/bernie.groovy
@@ -0,0 +1,114 @@
+job('checkout-ber') {
+  displayName('checkout ber')
+  description('use git to checkout Bernies branch from our gitlab server')
+  multiscm {
+    git {
+      branch('bernie-changes-2022-01-14.update.withmaster')
+      remote {
+        name('origin')
+        url('ssh://git@charmm-dev.org:65492/bucknerj/dev-release')
+        credentials('git')
+      }
+    }
+  }
+  triggers {
+    scm('@daily')
+  }
+  publishers {
+    mailer('bucknerj@umich.edu', true, true)
+  }
+} // end checkout job
+
+def cmakeBuilds =
+  [ [name: 'lite', build: '--lite -g', test: 'cmake']
+  , [name: 'openmm', build: '--with-fftdock', test: 'cmake']
+  , [name: 'domdec_gpu', build: '-u --with-gcc --with-fftdock', test: 'M 2 X 2 cmake']
+  , [name: 'blade', build: '-u --with-blade --with-gcc', test: 'cmake']
+  , [name:'intel', build:'--with-intel', test:'M 2 X 2 cmake']
+  , [name:'sccdftb' , build:'--with-sccdftb' , test:'cmake']
+  , [name:'repdstr' , build:'--with-repdstr' , test:'M 2 X 2 cmake']
+  , [name:'stringm', build:'--with-stringm', test:'M 8 X 2 cmake']
+  , [ name:'misc'
+    , build:'-a ABPO,ADUMBRXNCOR,ROLLRXNCOR,CORSOL,CVELOCI,PINS,ENSEMBLE,SAMC,MCMA,GSBP,PIPF,POLAR,PNM,RISM,CONSPH,RUSH,TMD,DIMS,MSCALE,EDS'
+    , test:'M 2 X 2 cmake'
+    ]
+  , [ name:'misc2'
+    , build:'--without-domdec --with-g09 -a DISTENE,MTS'
+    , test:'M 2 X 2 cmake'
+    ]
+  , [name:'tamd', build:'--without-mpi -a TAMD', test:'cmake']
+  , [name: 'mndo97', build: '--with-mndo97', test: 'cmake']
+  , [name: 'gamus', build: '--with-gamus' , test: 'cmake']
+  , [name: 'squantm', build: '--with-squantm', test: 'cmake']
+  , [ name:'pgi', build:'--with-pgi --without-openmm --without-mpi', test:'cmake' ]
+  , [ name:'ljpme', build:'--with-ljpme', test:'M 2 X 2 cmake' ]
+  ];
+
+// cmake jobs
+cmakeBuilds.each {
+  def current = it
+// begin build
+job("build-ber-${current.name}") {
+  displayName("build ber ${current.name}")
+  description("${current.name}\n${current.build}\n${current.test}")
+  multiscm {
+    git {
+      branch('master')
+      remote {
+        name('origin')
+        url('/opt/git/jenkins.git')
+      }
+      extensions {
+        relativeTargetDirectory('config')
+      }
+    }
+  }
+  triggers {
+    upstream('checkout-ber')
+  }
+  steps {
+    shell("/bin/bash -e config/scripts/cmake_build.bash ${current.build}")
+  }
+  publishers {
+    mailer('bucknerj@umich.edu', true, true)
+  }
+} // end build
+
+// begin test
+job("test-ber-${current.name}") {
+  displayName("test ber ${current.name}")
+  description("${current.name}\n${current.build}\n${current.test}")
+  multiscm {
+    git {
+      branch('master')
+      remote {
+        name('origin')
+        url('/opt/git/jenkins.git')
+      }
+      extensions {
+        relativeTargetDirectory('config')
+      }
+    }
+  }
+  triggers {
+    upstream("build-ber-${current.name}")
+  }
+  steps {
+    shell("/bin/bash config/scripts/test.bash ${current.test}")
+  }
+  publishers {
+    archiveXUnit {
+      jUnit {
+          pattern('xml/c*test.xml')
+      }
+      skippedThresholds {
+        failure(80)
+        failureNew(80)
+        unstable(50)
+        unstableNew(50)
+      }
+      thresholdMode(ThresholdMode.PERCENT)
+    }
+  }
+} // end test
+} // end cmake
