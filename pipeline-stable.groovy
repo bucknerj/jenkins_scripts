7b6c8a31472a85fca19c5f3233415509717f1ad2
Josh Buckner <bucknerj@umich.edu>
2025-01-29 11:29:39 -0500

    add parallel builds and tests to pipeline stable

--- pipeline-stable.groovy
+++ pipeline-stable.groovy
@@ -1,3 +1,36 @@
+charmmConfigs = [
+    'lite': '--lite',
+    'openmm': '--with-fftdock',
+    'domdec_gpu': '-u --with-fftdock',
+    'blade': '-u --with-blade',
+    'sccdftb': '--with-sccdftb',
+    'repdstr': '--with-repdstr',
+    'stringm': '--with-stringm',
+    'misc': '-a ABPO,ADUMBRXNCOR,ROLLRXNCOR,CORSOL,CVELOCI,PINS,ENSEMBLE,SAMC,MCMA,GSBP,PIPF,POLAR,PNM,RISM,CONSPH,RUSH,TMD,DIMS,MSCALE,EDS',
+    'misc2': '--without-domdec --with-g09 -a DISTENE,MTS',
+    'tamd': '--without-mpi -a TAMD',
+    'mndo97': '--with-mndo97',
+    'gamus': '--with-gamus' ,
+    'squantm': '--with-squantm',
+    'ljpme': '--with-ljpme'
+]
+
+charmmTests = [
+    'openmm': 'cmake',
+    'domdec_gpu': 'M 2 X 2 cmake',
+    'blade': 'cmake',
+    'sccdftb': 'cmake',
+    'repdstr': 'M 2 X 2 cmake',
+    'stringm': 'M 8 X 2 cmake',
+    'misc': 'M 2 X 2 cmake',
+    'misc2': 'M 2 X 2 cmake',
+    'tamd': 'cmake',
+    'mndo97': 'cmake',
+    'gamus': 'cmake',
+    'squantm': 'cmake',
+    'ljpme': 'M 2 X 2 cmake'
+]
+
 pipeline {
     agent any
     stages {
@@ -6,95 +39,122 @@ pipeline {
 		git branch: 'stable-release', url: 'gitlab:/bucknerj/dev-release'
 	    }
 	}
-	stage('Configure') {
-	    steps {
-		echo 'Configuring...'
-		sh '''
-                  eval "$(/home/bucknerj/.local/bin/micromamba shell hook --shell zsh)"
-                  micromamba activate dev
-                  rm -rf build/cmake
-                  ./configure -g --with-ninja
-                '''
-		echo '...finished configuring'
+	parallel {
+	    charmmConfigs.each { name, configArgs ->
+		stage("Configure ${name}") {
+		    steps {
+			echo "Configuring ${name}..."
+			sh """
+                          eval "$(/home/bucknerj/.local/bin/micromamba shell hook --shell zsh)"
+                          micromamba activate dev
+                          if [[ ! -d install-${name} ]]; then
+                            tool/NewCharmmTree install-${name}
+                          fi
+                          pushd install-${name}
+                          rm -rf build/cmake
+                          ./configure --with-ninja ${configArgs}
+                          popd
+                        """
+			echo "...finished configuring ${name}"
+		    }
+		}
 	    }
 	}
-	stage('Build') {
-	    steps {
-		echo 'Building...'
-		sh '''
-                  eval "$(/home/bucknerj/.local/bin/micromamba shell hook --shell zsh)"
-                  micromamba activate dev
-                  ninja -C build/cmake install
-                '''
-		echo '...finished building'
+	parallel {
+	    charmmConfigs.each { name, configArgs ->
+		stage("Build ${name}") {
+		    steps {
+		        echo "Building ${name}..."
+			sh """
+                          eval "$(/home/bucknerj/.local/bin/micromamba shell hook --shell zsh)"
+                          micromamba activate dev
+                          pushd install-${name}
+                          ninja -C build/cmake install
+                          popd
+                        """
+			echo "...finished building ${name}"
+		    }
+		}
 	    }
 	}
-	stage('Test') {
-	    steps {
-		echo 'Testing...'
-		sh '''
-                  eval "$(/home/bucknerj/.local/bin/micromamba shell hook --shell zsh)"
-                  micromamba activate dev
-                  pushd test
-                  if [[ -d output ]]; then
-                    rm -rf old
-                    mkdir old
-                    cp -r output* old/
-                    rm -rf output*
-                  fi
-                  if [[ -d old ]]; then
-                    if [[ -f test.log ]]; then
-                      cp test.log old/
-                      rm test.log
-                    fi
-                    if [[ -f compare.log ]]; then
-                      cp compare.log old/
-                      rm compare.log
-                    fi
-                    if [[ -f diff.log ]]; then
-                      cp diff.log old/
-                      rm diff.log
-                    fi
-                    if [[ -f test_results.xml ]]; then
-                      cp test_results.xml old/
-                      rm test_results.xml
-                    fi
-                  fi
-                  ./test.com cmake output old/output &> test.log
-                  popd
-                '''
-		echo '...finished testing'
+	parallel {
+	    charmmTests.each { name, testArgs ->
+		stage("Test ${name}") {
+		    steps {
+			echo "Testing ${name}..."
+			sh """
+                          eval "$(/home/bucknerj/.local/bin/micromamba shell hook --shell zsh)"
+                          micromamba activate dev
+                          pushd install-${name}/test
+                          if [[ -d output ]]; then
+                            rm -rf old
+                            mkdir old
+                            cp -r output* old/
+                            rm -rf output*
+                          fi
+                          if [[ -d old ]]; then
+                            if [[ -f test.log ]]; then
+                              cp test.log old/
+                              rm test.log
+                            fi
+                            if [[ -f compare.log ]]; then
+                              cp compare.log old/
+                              rm compare.log
+                            fi
+                            if [[ -f diff.log ]]; then
+                              cp diff.log old/
+                              rm diff.log
+                            fi
+                            if [[ -f test_results.xml ]]; then
+                              cp test_results.xml old/
+                              rm test_results.xml
+                            fi
+                          fi
+                          ./test.com ${testArgs} output old/output &> test.log
+                          popd
+                        """
+        		echo "...finished testing ${name}"
+        	    }
+        	}
 	    }
 	}
-	stage('Compare') {
-	    steps {
-		echo 'Comparing...'
-		sh '''
-                  eval "$(/home/bucknerj/.local/bin/micromamba shell hook --shell zsh)"
-                  micromamba activate dev
-                  pushd test
-                  export CMPDIR=old/output
-                  CMPDIR=old/output ../tool/Compare out put &> compare.log
-                  CMPDIR=old/output ../tool/Compare out put v &> diff.log
-                  popd
-                '''
-		echo '...finished comparing'
+	parallel {
+	    charmmTests.each { name, testArgs ->
+		stage("Compare ${name}") {
+		    steps {
+			echo "Comparing ${name}..."
+			sh """
+                          eval "$(/home/bucknerj/.local/bin/micromamba shell hook --shell zsh)"
+                          micromamba activate dev
+                          pushd install-${name}/test
+                          export CMPDIR=old/output
+                          CMPDIR=old/output ../tool/Compare out put &> compare.log
+                          CMPDIR=old/output ../tool/Compare out put v &> diff.log
+                          popd
+                        """
+        		echo "...finished comparing ${name}"
+        	    }
+        	}
 	    }
 	}
-	stage('Grade') {
-	    steps {
-		echo 'Grading...'
-		sh '''
-                  eval "$(/home/bucknerj/.local/bin/micromamba shell hook --shell zsh)"
-                  micromamba activate dev
-                  pushd test
-                  # call python script here
-                  python ../config/new-test-grader.py &> test_results.xml
-                  popd
-                '''
-		junit 'test/test_results.xml'
-		echo '...finished grading'
-	    }
+	parallel {
+	    charmmTests.each { name, testArgs ->
+                stage("Grade ${name}") {
+        	    steps {
+        		echo "Grading ${name}..."
+        		sh """
+                          eval "$(/home/bucknerj/.local/bin/micromamba shell hook --shell zsh)"
+                          micromamba activate dev
+                          pushd install-${name}/test
+                          # call python script here
+                          python ../config/new-test-grader.py &> test_results.xml
+                          popd
+                        """
+        		junit "install-${name}/test/test_results.xml"
+        		echo "...finished grading ${name}"
+        	    }
+        	}
+            }
 	}
     }
 }
